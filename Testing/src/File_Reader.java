a7c37a7029004d4861cfe93de6fb3d8c9f7b6ca3
rameshkarki <rameshkarki@localhost>
2021-08-25 21:34:12 -0400

    Added text of top laners

--- Testing/src/File_Reader.java
+++ Testing/src/File_Reader.java
@@ -6,10 +6,20 @@ public class File_Reader {
 	public static void File_Reader(String file_Name) throws FileNotFoundException {
 		File file = new File(file_Name);
 		Scanner scan = new Scanner(file);
+		Scanner sizeCheck = new Scanner(file);
+		int i = 0;
+		while (sizeCheck.hasNext()) {
+			String record = scan.nextLine();
+			Scanner sc = new Scanner(record);
+			sc.useDelimiter(",");
+			i+=1;			
+		}
+		System.out.println(i);
 		while (scan.hasNext()) {
 			String record = scan.nextLine();
 			Scanner sc = new Scanner(record);
 			sc.useDelimiter(",");
+			
 		}
 	}
 }
--- Testing/src/Testing.java
+++ Testing/src/Testing.java
@@ -1,7 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Testing {
-	public static void main (String args[]) {
+	public static void main (String args[]) throws FileNotFoundException {
 
-		GUI gui = new GUI();
+		//GUI gui = new GUI();
+		File f = new File("Top.txt");
+		Scanner sc = new Scanner(new File("/Users/rameshkarki/git/Test/Testing/src/Top.txt"));
+		sc.useDelimiter(",");
+		System.out.println(f.getName());
+		System.out.println(sc.hasNext());
+		while(sc.hasNext()) {
+			System.out.println("Try");
+			System.out.println(sc.nextLine());
+		}
+		/*String a = sc.nextLine();
+		System.out.println(a);
+		sc.useDelimiter(",");
+		while (sc.hasNext()) {
+			System.out.println(sc.next());
+		}*/
+		
 	}
 }
